e6e6867029d977949af67ddab664799b2050a7cc
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-12-04 18:24:17 -0300

    Se agrego imagenes de bootstrap y excel. Se modifico el css del footer. Se modifico el html del body.

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -22,15 +22,20 @@ export class BodyComponent {
       { path: this.IMAGEN_FOLDER + "icon-js.svg", descripcion: "Icono de Js", nombre: "Js" },
       { path: this.IMAGEN_FOLDER + "icon-nodejs.svg", descripcion: "Icono de NodeJs", nombre: "NodeJs" },
       { path: this.IMAGEN_FOLDER + "icon-java.svg", descripcion: "Icono de Java", nombre: "Java" },
-    {path: this.IMAGEN_FOLDER + "icon-spring-boot.svg", descripcion: "Icono de Spring Boot", nombre: "Spring Boot" }];
+    {path: this.IMAGEN_FOLDER + "icon-spring-boot.svg", descripcion: "Icono de Spring Boot", nombre: "Spring Boot" },
+    {path: this.IMAGEN_FOLDER + "icon-bootstrap.svg", descripcion: "Icono de bootstrap", nombre: "Bootstrap" },
+    {path: this.IMAGEN_FOLDER + "icon-excel.svg", descripcion: "Icono de excel", nombre: "Excel" }];
    
     this.proyectos = [{
-      descripcion: "Este es un proyecto de la facultad el cual trata de un tienda de mascotas. El proyecto fue realizado en Spring Boot",
+      descripcion: "Este es un proyecto que realize para una materia de la facultad el cual trata de una pagina web de una tienda de mascotas. El proyecto fue realizado en Spring Boot.",
       url: "https://github.com/CachaSan10/repositorio-tp2"
     }, {
-      descripcion: "Esta es un proyecto de la facultad es cual trata de una pagina de salud. El proyecto fue realizado en Spring Boot",
+      descripcion: "Este es un proyecto que realize para una materia de la facultad es cual trata de una pagina web de salud. El proyecto fue realizado en Spring Boot.",
       url: "https://github.com/CachaSan10/TP_FINAL_2023-PV"
-    }
+    },{
+      descripcion: "Este es el proyecto que realize para una materia de la facultad es cual trata de un juego de cartas por terminal. El proyecto fue realizado en C++.",
+        url: "https://github.com/CachaSan10/tp-final-ED-2023"
+      }
     ];
   }
 
